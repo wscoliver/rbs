eaa368076e136b1b90fa62661ad098a2adca59fd
Licera <wsoliver@gmail.com>
2014-02-19 07:25:33 -0500

    Post functionality added

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,7 @@
 //Include the nonblocking functionality
 var exec = require('child_process').exec;
+//Include the processing of text variable in postData
+var querystring =  require('querystring');
 
 function start(response, postData){
   console.log('Request handler "start" was called.');
@@ -7,24 +9,24 @@ function start(response, postData){
       '<head>'+
       '<meta http-equiv="Content-Type" content="text/html;'+
       'charset=UTF-8" />'+
-      '</head>/'+
+      '</head>'+
       '<body>'+
       '<form action="/upload" method="post">'+
       '<textarea name="text" rows="20" cols="60"></textarea>'+
       '<input type="submit" value="Submit Text" />'+
-      '</form>>'+
+      '</form>'+
       '</body>'+
       '</html>';
   response.writeHead(200,{"Content-Type":"text/html"});
   response.write(body);
   response.end();  
 
-});
+
 }
 function upload(response,postData){
   console.log('Request handler "upload" was called.');
   response.writeHead(200, {'Content-Type':'text/plain'});
-  response.write('You have sent : '+postData);
+  response.write('You have sent : '+querystring.parse(postData).text);
   response.end();
 }
 
